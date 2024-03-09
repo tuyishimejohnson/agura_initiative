811986c40dc395ab789e92193a7da59f27d19936
tuyishimejohnson <j.tuyishime4@alustudent.com>
2024-03-09 23:33:47 +0200

    Added the services page.

--- agura/agura_web_app/src/services.js
+++ agura/agura_web_app/src/services.js
@@ -12,11 +12,21 @@ const Services = () => {
 
   return (
     <div className='' ref={headerRef} style={{paddingTop: '3rem'}}>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, consequuntur itaque incidunt minus, quo quod amet neque, ipsum numquam eum dolore quidem non ullam. Dolorum architecto excepturi esse doloremque asperiores.</p>
-
-      <div className=''>
-        <img src={require('./1700218492637.jpg')} alt="" />
+      <div className='flex space-x-6'>
+        <div className='border-b-2'>
+          <img src={require('./socialMedia.jpg')} alt="" className='rounded-t-2xl'/>
+          <p className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe eligendi eos error nemo ipsam velit, et ratione culpa molestiae repudiandae quae fuga odit, exercitationem similique, suscipit quaerat maxime amet iusto?</p>
+        </div>
+        <div className='border-b-2'>
+          <img src={require('./socialMedia.jpg')} alt="" className='rounded-t-2xl'/>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum aspernatur minus impedit optio asperiores ullam fuga laudantium labore ab hic, totam vero explicabo, et amet veritatis exercitationem, voluptatem cum laboriosam.</p>
+        </div>
+        <div className='border-b-2'>
+          <img src={require('./socialMedia.jpg')} alt="" className='rounded-t-2xl'/>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla quasi libero labore enim nobis consequuntur, exercitationem qui dolores sed. Cupiditate aliquam pariatur voluptas aliquid odio, maiores accusamus ipsum voluptatibus dolorum?</p>
+        </div>
       </div>
+      
     </div>
   )
 }
